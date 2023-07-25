aeec63d988acef859af4165449ab943938c065c6
Juan Camilo Robayo Torres <68972889+Michiiman@users.noreply.github.com>
2023-07-24 22:00:41 -0500

    modificaicon de archivos

--- C#/Ejercicio001C#/Program.cs
+++ C#/Ejercicio001C#/Program.cs
@@ -6,9 +6,23 @@ class Program{
     static bool a=true;
     static List<string[]> arr = new List<string[]>();
     static void Main(){
-        Console.WriteLine(menu());
-        agregar();
-        mostrarDatos();
+
+        while (a){
+            Console.WriteLine(menu());
+            Console.Write("Seleccione la opción que necesita: ");
+            string dato=Console.ReadLine();
+            if (dato=="1"){
+                agregar();
+            }else if(dato=="2"){
+
+            }else if(dato=="3"){
+                mostrarDatos();
+            }else if(dato=="4"){
+                
+            }else if(dato=="5"){
+                a=false;
+            }
+    }
     }
     static string  menu(){
     string menu = "**** Menu ****\n1.Agregar.\n2.Eliminar.\n3.Ver lista.\n4.Editar.\n5.Salir.";
@@ -20,19 +34,20 @@ static void agregar(){
     string nombre= Console.ReadLine();
     Console.Write("Ingrese su telefono: ");
     string telefono= Console.ReadLine();
-    Console.WriteLine($"Su nombre es: {nombre} y su telefono es: {telefono}");
-    //arr.Add(new string[] {nombre,telefono});
+    arr.Add(new string[] {nombre,telefono});
 }
-static string mostrarDatos(){
-    arr.ForEach(e => {
-        foreach(string i in e ){
-            Console.WriteLine(i);
+static void mostrarDatos()
+    {
+        Console.WriteLine("Nombre          Telefono");
+        for (int i = 0; i < arr.Count; i++)
+        {
+            string[] datos = arr[i];
+            string nombre = datos[0];
+            string telefono = datos[1];
+            Console.WriteLine($"{i+1}.{nombre}, Teléfono: {telefono}");
         }
-    });
-    return "holis";
-}   
-
+    }
 }
 
-//Funciones
+
 
